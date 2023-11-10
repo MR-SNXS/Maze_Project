cba566105c4fe9d57411bdc74e8a8a61452246c4
MR-SNXS <141773024+MR-SNXS@users.noreply.github.com>
2023-11-10 10:32:41 -0600

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -15,6 +15,24 @@ public class MazeSolver {
         } else {
             if (maze.validPosition(startRow, startColumn)) {
                 maze.tryPosition(startRow, startColumn);
+                if(maze.getDirections()==4) {
+                	if (!done && !path.equals("D")) {
+                        path = "U";
+                        done = resolverLaberinto(startRow - 1, startColumn); // up
+                    }
+                    if (!done && !path.equals("R")) {
+                        path = "L";
+                        done = resolverLaberinto(startRow, startColumn - 1); // left
+                    }
+                    if (!done && !path.equals("U")) {
+                        path = "D";
+                        done = resolverLaberinto(startRow + 1, startColumn); // down
+                    }
+                    if (!done && !path.equals("L")) {
+                        path = "R";
+                        done = resolverLaberinto(startRow, startColumn + 1); // right
+                    }
+                }else if(maze.getDirections()==8) {
                 if (!done && !path.equals("D")) {
                     path = "U";
                     done = resolverLaberinto(startRow - 1, startColumn); // up
@@ -31,6 +49,24 @@ public class MazeSolver {
                     path = "R";
                     done = resolverLaberinto(startRow, startColumn + 1); // right
                 }
+                if(!done && !path.equals("DL")) {
+                	path = "UR";
+                	done = resolverLaberinto(startRow, startColumn + 1); //UR
+                }
+                if(!done && !path.equals("DR")) {
+                	path = "UL";
+                	done = resolverLaberinto(startRow - 1, startColumn - 1); //UL
+                }
+                if(!done && !path.equals("UR")) {
+                	path = "DL";
+                	done = resolverLaberinto(startRow + 1, startColumn - 1); //UL
+                }
+                if(!done && !path.equals("UL")) {
+                	path = "DR";
+                	done = resolverLaberinto(startRow + 1, startColumn + 1); //UL
+                }
+                }
+                
                 if (done) {
                     maze.markPath(startRow, startColumn, path);
                 }
